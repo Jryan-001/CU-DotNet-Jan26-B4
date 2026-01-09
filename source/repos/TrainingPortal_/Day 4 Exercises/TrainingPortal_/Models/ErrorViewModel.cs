22dec5c4e70566d67c80c8c47abdc5a16ef80666
JAI AGGARWAL <166898970+Jryan-001@users.noreply.github.com>
2026-01-09 23:24:40 +0530

    Delete source/repos/TrainingPortal_/Day 4 Exercises directory

--- source/repos/TrainingPortal_/Day 4 Exercises/TrainingPortal_/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TrainingPortal_.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
