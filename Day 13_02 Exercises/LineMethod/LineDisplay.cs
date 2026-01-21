8a09b3aac2cee20048a113669208a472f9241218
JAI AGGARWAL <jaiaggarwalatmzn01@gmail.com>
2026-01-21 16:15:42 +0530

    Daily Exercises

---
+++ Day 13_02 Exercises/LineMethod/LineDisplay.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemO
+{
+    internal class LineDisplay
+    {
+        //static void display()
+        //{
+        //    Console.WriteLine($"\nPrint in line: '-' ");
+        //    for (int i = 0; i < 40; i++)
+        //    {
+        //        Console.Write('-');
+        //    }
+        //}
+        //static void display(char disp)
+        //{
+        //    Console.WriteLine($"\nPrint in line: '{disp}' ");
+        //    for (int i = 0; i < 40; i++)
+        //    {
+        //        Console.Write(disp);
+        //    }
+        //}
+        //static void display(char disp, int f)
+        //{
+        //    Console.WriteLine($"\nPrint in line: '{disp}' ");
+        //    for (int i = 0; i < f; i++)
+        //    {
+        //        Console.Write(disp);
+        //    }
+        //}
+
+        static void display(char disp = '-', int f = 40)
+        {
+            Console.WriteLine($"\nPrint in line:{disp}");
+            for (int i = 0; i < f; i++)
+            {
+                Console.Write(disp);
+            }
+        }
+
+        static void InParameterMethod(in int f) 
+        {
+         //   f++; // read only
+        }
+
+
+
+        //static void display(char disp = '-', int f = 40, double d = 10.00, int a=4, char val='r')
+        //{
+        //    Console.WriteLine($"\nPrint in line:{disp}");
+        //    for (int i = 0; i < f; i++)
+        //    {
+        //        Console.Write($"{disp} {f} {d} {a} {val}");
+        //    }
+        //}
+        static void Main(string[] args)
+        {
+            int a = 10;
+            InParameterMethod(a);
+            //multiple options to call the method
+            display(); //don't pass anything as arguement
+            display('+');  //pass a character like +
+
+            display(f: 60);  //if the pattern of arguements is char then num like [display(char disp , int f )]
+                           //then if we want to change the int value from 40 to 60 or something then i must pass
+                           //the named arguement like the name of variable then use " : " as used here 
+                           // it will define that we mean the int value not the string or char its called named arguement pass
+            display('$', 61);  //pass character $ and a number like 60  //print the $ - 60 times
+        }
+    }
+}
---
+++ Day 14  Exercises/EmployeeClass/PersonClass.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TextAnalysisUtility
+{
+    class Employee
+    {
+        int id;
+
+        public void SetID(int id)
+        {
+            this.id = id; 
+        }
+
+        public string Name { get; set; }
+
+
+        private string department;
+
+        public string Department
+        {
+            get { return department; }
+            set { if(value == "Account" || value == "Sales" || value == "IT")
+                { department = value; }
+                else { department = "Not Listed"; }
+            }
+        }
+
+        private int salary;
+
+        public int Salary
+        {
+            get { return salary; }
+            set { if(value >= 50000 && value <= 90000) 
+                    salary=value; }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"ID of Employee: {id}");
+            Console.WriteLine($"Name of Employee: {Name}");
+            Console.WriteLine($"Department of Employee: {department}");
+            Console.WriteLine($"Salary of Employee: {salary}");
+        }
+
+
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e = new Employee();
+            Employee e1 = new Employee();
+            e.SetID(10);
+            e.Name = "Harpu";
+            e.Department = "Marketing";
+            e.Salary = 50001;
+            e.Display();
+
+            e1.SetID(15);
+            e1.Name = "Rahul";
+            e1.Department = "Account";
+            e1.Salary = 90001;
+            e1.Display();
+            
+        }
+    }
+}
