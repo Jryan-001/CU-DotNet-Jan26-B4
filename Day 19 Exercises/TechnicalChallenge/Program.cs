347806d9fccfd6236397e243a99fdb38da6acb1b
JAI AGGARWAL <jaiaggarwalatmzn01@gmail.com>
2026-01-29 09:36:37 +0530

    Daaily Exercise

---
+++ Day 19 Exercises/TechnicalChallenge/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace TechnicalChallenge
+{
+
+    abstract class vehicle
+    {
+        public string ModelName { get; set; }
+        public abstract void Move();
+
+        public virtual string GetFuelStatus()
+        {
+            return $"Fuel level is stable";
+        }
+    }
+
+    class ElectricCar : vehicle
+    {
+        public override void Move()
+        {
+            Console.WriteLine($"{ModelName} is gliding silently on battery power.");
+        }
+
+        public override string GetFuelStatus()
+        {
+            return $"{ModelName} battery is at 80%.";
+        }
+    }
+
+    class HeavyTruck : vehicle
+    {
+        public override void Move()
+        {
+            Console.WriteLine($"{ModelName} is gliding silently on battery power.");
+        }
+    }
+
+    class CargoPlane : vehicle
+    {
+        public override void Move()
+        {
+            Console.WriteLine($"{ModelName} is ascending to 30,000 feet.");
+        }
+
+        public override string GetFuelStatus()
+        {
+            return base.GetFuelStatus() + $" Checking jet fuel reserves...";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            vehicle[] v1 = new vehicle[]
+            {
+                new ElectricCar(){ ModelName = "koenigsegg"},
+                new HeavyTruck(){ ModelName = "Maserati"},
+                new CargoPlane(){ ModelName = "AlpineOne"}
+            };
+            foreach (var v in v1)
+            {
+                v.Move();
+                Console.WriteLine(v.GetFuelStatus());
+            }
+        }
+    }
+}
---
+++ Day 19 Exercises/UtilityBillingSystem/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace UtilityBillingSystem
+{
+    abstract class UtilityBill
+    {
+        public int ConsumerId { get; set; }
+        public string ConsumerName { get; set; }
+        public decimal UnitsConsumed { get; set; }
+        public decimal RatePerUnit { get; set; }
+        protected UtilityBill(int id, string name, decimal unit, decimal rate)
+        {
+            ConsumerId = id;
+            ConsumerName = name;
+            UnitsConsumed = unit;
+            RatePerUnit = rate;
+        }
+        public abstract decimal CalculateBillAmount();
+        public virtual decimal CalculateTax(decimal billAmount)
+        {
+            return billAmount * 0.05m;
+        }
+        public void PrintBill()
+        {
+            decimal total = CalculateBillAmount() + CalculateTax(CalculateBillAmount());
+            Console.WriteLine($"Consumer ID: {ConsumerId}");
+            Console.WriteLine($"Consumer Name: {ConsumerName}");
+            Console.WriteLine($"Total Units: {UnitsConsumed}");
+            Console.WriteLine($"Final Payable amount: {total}");
+        }
+    }
+    class ElectricityBill : UtilityBill
+    {
+        public ElectricityBill(int id, string name, decimal unit, decimal rate) : base(id, name, unit, rate)
+        {
+            Console.WriteLine("In the electricty bill Constructor");
+        }
+        public override decimal CalculateBillAmount()
+        {
+            decimal amount = UnitsConsumed * RatePerUnit;
+            if (UnitsConsumed > 300)
+            {
+                amount += amount * 0.10m;
+            }
+            return amount;
+        }
+    }
+    class WaterBill : UtilityBill
+    {
+        public WaterBill(int id, string name, decimal unit, decimal rate) : base(id, name, unit, rate)
+        {
+            Console.WriteLine("In the Water bill Constructor");
+        }
+        public override decimal CalculateBillAmount()
+        {
+            return UnitsConsumed * RatePerUnit;
+        }
+        public override decimal CalculateTax(decimal billAmount)
+        {
+            return billAmount * 0.02m;
+        }
+    }
+
+    class GasBill : UtilityBill
+    {
+        public GasBill(int id, string name, decimal unit, decimal rate) : base(id, name, unit, rate)
+        {
+            Console.WriteLine("In the Gas bill Constructor");
+        }
+        public override decimal CalculateBillAmount()
+        {
+            return (UnitsConsumed * RatePerUnit) + 150;
+        }
+        public override decimal CalculateTax(decimal billAmount)
+        {
+            return 0m;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<UtilityBill> list = new List<UtilityBill>
+            {
+                new ElectricityBill(10001, "Ajay", 252, 7.7m),
+                new WaterBill(10005, "Birat", 209, 7.6m),
+                new GasBill(10008, "Chandu", 50, 8.9m)
+            };
+            foreach (UtilityBill bill in list)
+            {
+                bill.PrintBill();
+            }
+        }
+    }
+}
